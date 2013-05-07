81e3abb5383c581adb3396c3f98c3fd94db06249
marcelo <marcelo@manguaba>
2013-05-07 16:06:18 -0300

    package changed

---
+++ src/com/marcelodamasceno/experiments/KNN.java
@@ -0,0 +1,31 @@
+package com.marcelodamasceno.experiments;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.lazy.IBk;
+
+import com.marcelodamasceno.main.InterSession;
+import com.marcelodamasceno.main.InterWeek;
+import com.marcelodamasceno.main.IntraSession;
+
+public class KNN {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		KNN knn=new KNN();
+		knn.executeExperiments();
+	}
+	
+	private void executeExperiments(){
+		InterSession interSession=new InterSession();
+		InterWeek interWeek = new InterWeek();
+		IntraSession intraSession=new IntraSession();
+		
+		Classifier ibk=new IBk(5);
+		interWeek.classifyAllUsers(ibk,false, false);
+		interSession.classifyAllUsers(ibk, false, false);
+		intraSession.classifyAllUsers(ibk, false, false);
+	}
+
+}
