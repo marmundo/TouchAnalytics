3f2c5ba41077cbc8c01015639bd647f784f9609b
marcelo <marcelo@manguaba>
2013-05-29 11:03:26 -0300

    execute method finished

--- src/com/marcelodamasceno/utils/GramSchmidt.java
+++ src/com/marcelodamasceno/utils/GramSchmidt.java
@@ -25,8 +25,8 @@ public class GramSchmidt {
 	 * @param v
 	 * @return the projected vector
 	 */
-	private Matrix proj(Matrix u, Matrix v){		
-		double temp=Matriz.innerProduct(u.transpose(), v);
+	private Matrix proj(Matrix u, Matrix v){
+		double temp=Matriz.innerProduct(u.transpose(), v);		
 		temp=temp/Matriz.innerProduct(u.transpose(), u);		
 		return  u.times(temp);
 	}
@@ -37,19 +37,32 @@ public class GramSchmidt {
 		int nrow=dataset.getRowDimension()-1;
 		int ncol=dataset.getColumnDimension()-1;
 		Matrix u= new Matrix(nrow+1, ncol+1);
-		Matrix v= dataset;			
-		u.setMatrix(0, 0, 0, ncol, dataset.getMatrix(0, 0, 0, ncol));		
-		Matrix temp=new Matrix(1, ncol+1);		
+		Matrix v= dataset;
+		//doing a copy of dataset
+		u.setMatrix(0, 0, 0, ncol, dataset.getMatrix(0, 0, 0, ncol));	
+		//A matrix with one line
+		Matrix temp=new Matrix(1, ncol+1);
+		//
 		for(int i=1;i<nrow;i++){
+			//making the projection for each vector (line) of the data set
 			for(int j=0;j<i;j++){				
 				temp=proj(u.getMatrix(j, j, 0, ncol), v.getMatrix(i, i, 0, ncol));				
 				temp=temp.plus(temp);
 			}						
-			temp=v.getMatrix(i, i, 0, ncol).minus(temp);			
-			temp=temp.times(Math.pow(temp.norm1(),-1.0));			
+			//doing the orthogonalization
+			temp=v.getMatrix(i, i, 0, ncol).minus(temp);
+			//doing the orthomalization
+			temp=temp.times(Math.pow(temp.norm1(),-1.0));	
+			//putting the new vector into u
 			u.setMatrix(i, i, 0, ncol, temp);				
 		}		
 		return u;
+	}
+	
+	public Instances execute(){		
+		Matrix matrix=orthogonalization();
+		Transformations t =new Transformations();
+		return t.MatrixtoInstances(matrix);
 	}
 
 	
