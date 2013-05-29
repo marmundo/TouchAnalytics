03e27fa495ee560fd40ee379df0185097eb6c363
marcelo <marcelo@manguaba>
2013-05-29 09:46:20 -0300

    MatrixtoInstance method added

--- src/com/marcelodamasceno/utils/Transformations.java
+++ src/com/marcelodamasceno/utils/Transformations.java
@@ -1,6 +1,12 @@
 package com.marcelodamasceno.utils;
 
+import java.util.ArrayList;
+
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.matrix.Matrix;
 
 /**
  * Class that saves methods to convert differents objects in another compatives objects
@@ -24,4 +30,26 @@ public class Transformations {
 		return double_dataset;
 	}	
 
+	/**
+	 * Convert a Matrix object in a Instance object
+	 * @param matrix
+	 * @return
+	 */
+	public Instances MatrixtoInstances(Matrix matrix){		
+		int m=matrix.getColumnDimension();
+		ArrayList<Attribute> attributeList=new ArrayList<Attribute>(m);
+		for(int i=0;i<m;i++){
+			Attribute attr = new Attribute("m"+String.valueOf(i));
+			attributeList.add(attr);			
+		}
+		Instances inst=new Instances("dataset", attributeList, m);
+		for(int i=0;i<matrix.getRowDimension();i++){			
+			Instance instance=new DenseInstance(m);
+			for(int a=0;a<m;a++){
+				instance.setValue(attributeList.get(a), matrix.get(i, a));
+			}
+			inst.add(instance);
+		}
+		return inst;
+	}
 }
