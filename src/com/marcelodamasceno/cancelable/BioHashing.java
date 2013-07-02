cab58371eb6c533b88c5937d8319e20073b533f8
marcelo <marcelo@manguaba>
2013-07-02 15:08:27 -0300

    discretization(Instances data) added

--- src/com/marcelodamasceno/cancelable/BioHashing.java
+++ src/com/marcelodamasceno/cancelable/BioHashing.java
@@ -5,100 +5,139 @@ import java.util.ArrayList;
 
 import com.marcelodamasceno.util.ArffConector;
 import com.marcelodamasceno.util.GramSchmidt;
+import com.marcelodamasceno.util.InstancesUtils;
 import com.marcelodamasceno.util.Matriz;
-
+import com.marcelodamasceno.util.Utils;
 
 import weka.core.Attribute;
 import weka.core.DenseInstance;
 import weka.core.Instance;
 import weka.core.Instances;
 
-public class BioHashing extends Cancelable{
+public class BioHashing extends Cancelable {
 
-	private Instances data;
-	public BioHashing(Instances data){
-		this.data=data;
+    private Instances dataset;
+
+    public BioHashing(Instances data) {
+	this.dataset = data;
+    }
+
+    /**
+     * Creates the cancelable biometric dataset using bioHashing
+     * 
+     * @param m
+     *            number of attributes of the cancelable biometric
+     * @return
+     */
+    public Instances generate() {
+	/* Creating the m attributes */
+	int m = dataset.numAttributes() - 1;
+	ArrayList<Attribute> attributeList = new ArrayList<Attribute>(m);
+	for (int i = 1; i <= m; i++) {
+	    Attribute attr = new Attribute("m" + String.valueOf(i));
+	    attributeList.add(attr);
 	}
-	
-	/**
-	 * Creates the cancelable biometric dataset using bioHashing
-	 * @param m number of attributes of the cancelable biometric
-	 * @return
-	 */
-	public Instances execute(int m){
-		/*Creating the m attributes*/
-		ArrayList<Attribute> attributeList=new ArrayList<Attribute>(m);
-		for(int i=0;i<m;i++){
-			Attribute attr = new Attribute("m"+String.valueOf(i));
-			attributeList.add(attr);
-		}		 
-		Instances randowDataSet=new Instances("randow", attributeList, data.numInstances());
-		/*Gerando n vetores randomicos*/
-		for(int row=0;row<m;row++){
-			Instance inst=new DenseInstance(m);
-			for(int a=0;a<m;a++){
-				inst.setValue(attributeList.get(a), Math.random());
+	Instances randowDataSet = new Instances("randow", attributeList,
+		dataset.numInstances());
+	/* Gerando n vetores randomicos */
+	for (int row = 1; row <= m; row++) {
+	    Instance inst = new DenseInstance(m);
+	    for (int a = 0; a < m; a++) {
+		inst.setValue(attributeList.get(a), Math.random());
+	    }
+	    randowDataSet.add(inst);
+	}
+	/* Aplicando o processo de Gram-Schimdth */
+	GramSchmidt gram = new GramSchmidt(randowDataSet);
+	Instances orthonormalInstances = gram.execute();
+	// randowDataSet=null;
+
+	Attribute classe = dataset.classAttribute();
+	Instances copyDataset = new Instances(dataset);
+	copyDataset.setClassIndex(copyDataset.numAttributes() - 2);
+	copyDataset.deleteAttributeAt(copyDataset.numAttributes() - 1);
+
+	/* Produto interno entre o dataset biométrico e o vetor orthornormal */
+	Instances product = Matriz.innerProduct(copyDataset,
+		orthonormalInstances);
+	product = discretization(product, 0.55);
+	product.insertAttributeAt(classe, product.numAttributes());
+	InstancesUtils.copyAttributeValue(dataset, dataset.classAttribute()
+		.index(), product, product.numAttributes() - 1);
+
+	return product;
+    }
+
+    /**
+     * Discretizes the dataset to 1 when the value is higher than thereshold,
+     * else ortherwise
+     * 
+     * @param data
+     *            Instances that will be discretized
+     * @param thereshold
+     *            Thereshold
+     * @return
+     */
+    private Instances discretization(Instances data, double thereshold) {
+	for (int i = 0; i < data.numInstances(); i++) {
+	    for (int j = 0; j < data.numAttributes(); j++) {
+		if (data.get(i).value(j) > thereshold) {
+		    data.get(i).setValue(j, 1.0);
+		} else {
+		    data.get(i).setValue(j, 0.0);
+		}
+	    }
+	}
+	return data;
+    }
+
+    /**
+     * Discretizes the dataset using 4 thereshold (0, 0.33, 0.66, 1)
+     * @param data
+     * @return Discretized data
+     */
+    private Instances discretization(Instances data) {
+	for (int i = 0; i < data.numInstances(); i++) {
+	    for (int j = 0; j < data.numAttributes(); j++) {
+		double instanceValue = data.get(i).value(j);
+		if (instanceValue < 0.25) {
+		    data.get(i).setValue(j, 0);
+		} else {
+		    if (instanceValue >= 0.25 && instanceValue < 0.5) {
+			data.get(i).setValue(j, 0.33);
+		    } else {
+			if (instanceValue >= 0.5 && instanceValue < 0.75) {
+			    data.get(i).setValue(j, 0.66);
+			} else {
+			    data.get(i).setValue(j, 1);
 			}
-			randowDataSet.add(inst);
+		    }
 		}
-		/*Aplicando o processo de Gram-Schimdth*/
-		GramSchmidt gram= new GramSchmidt(randowDataSet);
-		Instances orthonormalInstances=gram.execute();
-		//randowDataSet=null;
-		
-		
-		
-		
-		/*Produto interno entre o dataset biométrico e o vetor orthornormal*/
-		Instances product= Matriz.innerProduct(data, orthonormalInstances);
-		product=discretization(product, 0.55);
-		/*System.out.println("orto(m,n): ("+orthonormalInstances.numInstances()+" , "+orthonormalInstances.numAttributes()+")");
-		System.out.println("data(m,n): ("+data.numInstances()+" , "+data.numAttributes()+")");
-		System.out.println("product(m,n): ("+product.numInstances()+" , "+product.numAttributes()+")");
-		System.out.println(product);*/
-		return product;
+	    }
 	}
-	
-	/**
-	 * Discretizes the dataset to 1 when the value is higher than thereshold, else ortherwise
-	 * @param data Instances that will be discretized
-	 * @param thereshold Thereshold
-	 * @return
-	 */
-	private Instances discretization(Instances data,double thereshold){
-		for(int i=0;i<data.numInstances();i++){
-			for(int j=0;j<data.numAttributes();j++){
-				if(data.get(i).value(j)>thereshold){
-					data.get(i).setValue(j, 1.0);
-				}else{
-					data.get(i).setValue(j,0.0);
-				}
-			}
-		}
-		return data;
-	}
-	
+	return data;
+    }
 
-	/**
-	 * @param args
-	 * @throws FileNotFoundException 
-	 */
-	public static void main(String[] args){
-		
-		ArffConector conector=new ArffConector();
-		Instances dataset=null;
-		String projectPath="/home/marcelo/Área de Trabalho/Documentos-Windows/Google Drive/doutorado/projeto/dataset/Base de Toque/";		
-		String folderResults="IntraSession/";
-		
-		try {
-		    dataset = conector.openDataSet(projectPath+folderResults+"IntraSession-User_41_Day_1_Scrolling.arff");
-		} catch (FileNotFoundException e) {
-		    // TODO Auto-generated catch block
-		    e.printStackTrace();
-		}
-		BioHashing bio = new BioHashing(dataset);
-		bio.execute(dataset.numAttributes());
+    /**
+     * @param args
+     * @throws FileNotFoundException
+     */
+    public static void main(String[] args) {
 
+	ArffConector conector = new ArffConector();
+	Instances dataset = null;
+	String projectPath = "/home/marcelo/Área de Trabalho/Documentos-Windows/Google Drive/doutorado/projeto/dataset/Base de Toque/";
+	String folderResults = "IntraSession-SemNominal/";
+
+	try {
+	    dataset = conector.openDataSet(projectPath + folderResults
+		    + "IntraSession-User_41_Day_1_Scrolling.arff");
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
 	}
+	BioHashing bio = new BioHashing(dataset);
+	bio.generate();
+
+    }
 
 }
