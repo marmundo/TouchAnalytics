3583a025cb6023650cd9b68704a4ea50e0d6246d
marcelo <marcelo@manguaba>
2014-02-18 11:01:15 -0300

    Class of constants in the project

---
+++ src/com/marcelodamasceno/util/Const.java
@@ -0,0 +1,17 @@
+package com.marcelodamasceno.util;
+
+/**
+ * Class for Constants names
+ * @author marcelo
+ *
+ */
+public final class Const {
+    
+    public static final String INTERSESSION="intersession";
+    public static final String INTERWEEK="interweek";
+    public static final String INTRASESSION="intrasession";
+    public static final String INCORRECT="incorrect";
+    public static final String EER="eer";
+    
+
+}
