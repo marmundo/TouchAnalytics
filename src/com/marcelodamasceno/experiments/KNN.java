9dd729ce7e62733c635ee2708a8cf7706e78b7b0
marcelo <marcelo@manguaba>
2014-02-20 11:38:48 -0300

    String Constant was add and finished sound was added

--- src/com/marcelodamasceno/experiments/KNN.java
+++ src/com/marcelodamasceno/experiments/KNN.java
@@ -1,7 +1,10 @@
 package com.marcelodamasceno.experiments;
 
+import com.marcelodamasceno.util.Alarm;
+
 import weka.classifiers.Classifier;
 import weka.classifiers.lazy.IBk;
+import static com.marcelodamasceno.util.Const.*;
 
 /**
  * Class that implements KNN
@@ -20,14 +23,20 @@ public class KNN extends com.marcelodamasceno.experiments.Classifier {
     public static void main(String[] args) {
 	Classifier classifier = new IBk(5);
 	KNN knn = new KNN(classifier);
-	knn.executeInterpolationCancelaveis("Incorrect");
-	// knn.executeExperiments("EER");
-	// knn.executeBioHashingCancelaveis("EER");
-	// knn.executeBioHashingCancelaveis("Incorrect");
-	// knn.executeBioConvolvingCancelaveis("EER");
-	// knn.executeBioConvolvingCancelaveis("Incorrect");
-	// knn.executeDoubleSumCancelaveis("EER");
-	// knn.executeDoubleSumCancelaveis("Incorrect");
+	knn.executeInterpolationCancelaveis(INCORRECT);
+	// knn.executeExperiments(EER);
+	// knn.executeBioHashingCancelaveis(EER);
+	// knn.executeBioHashingCancelaveis(INCORRECT);
+	// knn.executeBioConvolvingCancelaveis(EER);
+	// knn.executeBioConvolvingCancelaveis(INCORRECT);
+	// knn.executeDoubleSumCancelaveis(EER);
+	// knn.executeDoubleSumCancelaveis(INCORRECT);
+	
+	try {
+	    Alarm.play();
+	} catch (final Exception e) {
+	    System.err.println("The sound is unable!");
+	}
     }
 
 }
