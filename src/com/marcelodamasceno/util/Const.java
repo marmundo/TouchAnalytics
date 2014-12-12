95f651f0145fde825d1f8809979f7b86f29507dc
marmundo <marcelodamasceno@gmail.com>
2014-12-12 14:04:37 +0000

    Some constants added

--- src/com/marcelodamasceno/util/Const.java
+++ src/com/marcelodamasceno/util/Const.java
@@ -17,5 +17,18 @@ public final class Const {
     public static final String PROJECTPATH="/home/marcelo/Área de Trabalho/projeto/dataset/Base de Toque/";
     public static final String USERPATH = System.getProperty("user.home") + File.separator+"TouchAnalytics"+File.separator+"Results";
     
+    public static final String INTERPOLATION="Interpolation";
+    public static final String DOUBLESUM="DoubleSum";
+    public static final String BIOCONVOLVING="BioConvolving";
+    public static final String BIOHASHING="BioHashing";
+    
+    public static final String DECISIONTREE="weka.classifiers.trees.J48";
+    public static final String SVM="weka.classifiers.functions.SMO";
+    public static final String MLP="weka.classifiers.functions.MultilayerPerceptron";
+    public static final String KNN="weka.classifiers.lazy.IBk";
+    
+    public static final String HORIZONTAL="Horizontal";
+    public static final String SCROOLING="Scrolling";
+    
 
 }
