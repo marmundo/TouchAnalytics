d98b534c571f8395646240701081eefe7cc2af6c
Marcelo Damasceno <marcelodamasceno@gmail.com>
2015-04-01 14:02:01 +0100

    Comments code

--- src/com/marcelodamasceno/methodology/Testing.java
+++ src/com/marcelodamasceno/methodology/Testing.java
@@ -6,15 +6,33 @@ import weka.classifiers.Classifier;
 import weka.core.Instance;
 import weka.core.Instances;
 
+/**
+ * Class test and generate scores matrix
+ * 
+ * @author marcelo
+ *
+ */
 public class Testing {
     private Classifier classifier;
     private Instances testDataSet;
 
+    /**
+     * Constructor
+     * 
+     * @param classifier Classifier used to test the {@code testDataSet}
+     * @param testDataset Instances will be tested
+     */
     public Testing(Classifier classifier, Instances testDataset){
 	this.classifier=classifier;
 	this.testDataSet=testDataset;
     }
 
+    /**
+     * Generates the score of the instances presented in Testing object.
+     * The scores is calculated using log(probability_class_1 / probability_class_2)
+     * @return prediction score
+     * @throws Exception
+     */
     public ArrayList<Double> getScores() throws Exception{
 	ArrayList<Double> scores=new ArrayList<Double>();
 	for (Instance testInstance : testDataSet) {	  
