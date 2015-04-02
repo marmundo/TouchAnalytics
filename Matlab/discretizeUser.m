a29e1a8d0f0348d69676b838f1960fe33ef50066
Marcelo Damasceno <marcelodamasceno@gmail.com>
2015-04-02 17:43:53 +0100

    fix bug

--- Matlab/discretizeUser.m
+++ Matlab/discretizeUser.m
@@ -1,17 +1,21 @@
-function biometricData=discretizeUser(user,userIndex,biometricData)
+function [biometricData, userLabels]=discretizeUser(user,userIndex,biometricData)
 %discretizeUser discretize the user to 1 and the remaining users in the
 %biometricData to 0
 %user= user label
 %user Index= position of user label in the data
 %biometricData=biometric data
 if ischar(user)
-    user=str2num(user)
+    user=str2double(user);
 end
  %% Changing the label of User to positive
- userLines=find(biometricData(:,userIndex)==user);
- biometricData(userLines,userIndex)='client';
+ userLabels=cell(length(biometricData(:,userIndex)),1);
+ userLabels(biometricData(:,userIndex)==user)={'client'};
+
+
 
  %% Changing the label of the remaining users to impostor
- userLines=find(biometricData(:,userIndex)~=user);
- biometricData(userLines,userIndex)='impostor';
+ userLabels(biometricData(:,userIndex)~=user)={'impostor'};
+ 
+ % Deleting the userLabels
+ biometricData=biometricData(:,2:end);
 end
