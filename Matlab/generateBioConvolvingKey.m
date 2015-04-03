46a121a08d151bc7cd827617a9d2ba784ddb35f1
Marcelo Damasceno <marcelodamasceno@gmail.com>
2015-04-03 16:28:19 +0100

    adds a function to create a key to BioConvolving cancelable function

---
+++ Matlab/generateBioConvolvingKey.m
@@ -0,0 +1,19 @@
+function [key]=generateBioConvolvingKey(w,n)
+%w= number of partitions. We consider the w as the size of the key
+%n= number of features of the biometric data
+  %% dj random numbers
+    
+    %random numbers between 1 and 99
+    r = (99-1).*rand(w-1,1) + 1;
+    d=sort(r);
+    
+    %% Putting 0 at index 1 and 100 in the last position
+    d(2:end+1)=d;
+    d(1)=0;
+    d(end+1)=100;
+    
+    n=length(biometric_data(1,:));
+    
+    key=round((d/100)*n);
+    
+end
