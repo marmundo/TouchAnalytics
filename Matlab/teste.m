4cfc89d3f196387c450c82f613d2aba9b97a6c29
marmundo <marcelodamasceno@gmail.com>
2015-04-10 15:47:59 +0100

    I use this file to create prototypes of scriopt or to call some functionalities of my matlab code

---
+++ Matlab/teste.m
@@ -0,0 +1,36 @@
+%% I use this file to create prototypes of scriopt or to call some functionalities of my
+%% matlab code
+
+
+%% This script loads the all the cancelable biometric data for all users using all the keys types in
+%% scrolling and horizontal stroke directions.
+
+% cancelableFunctions=char('Interpolation','DoubleSum','BioHashing','BioConvolving');
+% strokeOrientation=char('Scrolling','Horizontal');
+% key=char('Same_Key','Different_Key');
+% 
+%  
+% for keyIndex=1:length(key)
+%     for orienIndex=1:length(strokeOrientation)
+%         for cancIndex=1:length(cancelableFunctions)
+%             for user=1:41
+%                 % Loading training data
+%                 load(strcat(pwd(),'/Data/', strokeOrientation(orienIndex,:),'/',cancelableFunctions(cancIndex,:),'/',key(keyIndex,:),'/User_',num2str(user),'/trainingSet.mat'),'trainingSet','trainUserLabels');
+%                 
+%                 % Loading testData
+%                 load(strcat(pwd(),'/Data/', strokeOrientation(orienIndex,:),'/',cancelableFunctions(cancIndex,:),'/',key(keyIndex,:),'/User_',num2str(user),'/testSet.mat'),'testSet','testUserLabels');
+%                 
+%             end
+%         end
+%     end
+% end
+
+%% Creates all the cancelable data
+
+%for i=2:9
+%    main(i)
+%end
+
+%% Show a Norman's plot for a specific user using a specific orientation, keytype and biometric data
+
+main(10,'knn',1,'Interpolation','Same_Key','Scrolling')
