857bf07bc5b4d12301261c5199563696fcdf66cc
marmundo <marcelodamasceno@gmail.com>
2015-04-20 17:59:21 +0100

    adds scale procedure and grid search to find the best g and c parameters

--- Matlab/prediction.m
+++ Matlab/prediction.m
@@ -37,7 +37,20 @@ elseif strcmp(classifierName,'libsvm')
   %% Training
   clientProportion=num2str(sum(trainUserLabels==1)/size(trainUserLabels,1));
   impostorProportion=num2str(sum(trainUserLabels==-1)/size(trainUserLabels,1));
-  classifier = svmtrain(trainUserLabels, trainingDataSet,['-b 1 -w0 ',impostorProportion,' -w1 ',clientProportion]);
+  
+minimums = min(trainingDataSet, [], 1);
+ranges = max(trainingDataSet, [], 1) - minimums;
+
+trainingDataSet = (trainingDataSet - repmat(minimums, size(trainingDataSet, 1), 1)) ./ repmat(ranges, size(trainingDataSet, 1), 1);
+
+testDataSet = (testDataSet - repmat(minimums, size(testDataSet, 1), 1)) ./ repmat(ranges, size(testDataSet, 1), 1);
+
+save('trainingdata.scale','trainingDataSet');
+save('testdata.scale','testDataSet');
+[c,g]=grid(trainUserLabels, trainingDataSet);
+c=num2str(c);
+g=num2str(g);
+classifier = svmtrain(trainUserLabels, trainingDataSet,['-c ', c, ' -g ', g,' -b 1 -w-1 ',impostorProportion,' -w1 ',clientProportion]);
   
 elseif strcmp(classifierName,'discriminant')
   classifier=fitcdiscr(trainingDataSet,trainUserLabels);
