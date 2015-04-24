82aec0aa61c71851ea7a8c5d4ee68caeeb1a610b
Marcelo Damasceno de Melo <marcelo@Marcelos-MacBook-Pro.local>
2015-04-24 18:18:20 +0100

    closes #18

--- Matlab/generatingInterpolationTraining.m
+++ Matlab/generatingInterpolationTraining.m
@@ -9,14 +9,15 @@ function [inter_train]=generatingInterpolationTraining(trainingSet,user,saveFile
 
 %starting variable
 inter_train=[];
+numFeatures=length(trainingSet(1,:));
 
 if optionkey==1
     %% Same key for all users
-    key=[0.4942,0.2983,0.0715,0.9300,0.0240,0.1573,0.7370,0.2256,0.0763,0.0340,0.7855,0.1756,0.4953,0.4808,0.8389,0.9203,0.8051,0.3003,0.2362,0.1367,0.9224,0.4827,0.6794,0.1586,0.7258,0.2506,0.3365,0.5947,0.0483,0.6553,0.5235]';
+    key=getFixedKey('Interpolation',numFeatures);
     inter_train=interpolation(trainingSet(:,2:end),key);
 elseif optionkey==2
     %% Different key for each user
-    numFeatures=length(trainingSet(1,:));
+    
     users=unique(trainingSet(:,1));
     
     for currentUser=1:length(users)
