1e0462e0de967c1d142ebb35c814c260fa8fb990
Marcelo Damaceno <marcelodamasceno@gmail.com>
2015-05-07 17:13:14 +0100

    removes phoneId and docId

--- Matlab/cleaningdataset.m
+++ Matlab/cleaningdataset.m
@@ -2,4 +2,8 @@ function [dataset]=cleaningdataset(dataset)
 % This function remove Inf and NaN samples
 dataset(isnan(dataset)) = 0 ;
 dataset(isinf(dataset)) = 0 ;
+
+%removing phoneId,docId
+dataset(:,[13,2])=[];
+
 end
