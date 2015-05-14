558010be8fa4a7c0e3eee85d03c4582ecf6afcd7
Marcelo Damaceno <marcelodamasceno@gmail.com>
2015-05-14 15:01:03 +0100

    changes the number of folds to 3

--- Matlab/grid.m
+++ Matlab/grid.m
@@ -2,8 +2,8 @@ function [c,g]=grid(trainUserLabels, trainingDataSet)
 bestcv = 0;
 for log2c = -1:3,
   for log2g = -4:1,
-    cmd = ['-v 5 -c ', num2str(2^log2c), ' -g ', num2str(2^log2g)];
-    cv = svmtrain(trainUserLabels, trainingDataSet, cmd);
+    cmd = ['-v 3 -c ', num2str(2^log2c), ' -g ', num2str(2^log2g)];
+    cv = svmtrain(trainUserLabels,trainingDataSet,cmd);
     if (cv >= bestcv),
       bestcv = cv; bestc = 2^log2c; bestg = 2^log2g;
     end
