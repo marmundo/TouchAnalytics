ba279b364cb62e8e519077728a6437bafe4c1e49
Marcelo Damaceno <marcelodamasceno@gmail.com>
2015-05-19 19:37:26 +0100

    adds a example how to print DET curves

--- Matlab/teste.m
+++ Matlab/teste.m
@@ -63,16 +63,16 @@
 %main(10,'svm',1,'Interpolation','Same_Key','Scrolling')
 
 % Show a Norman's plot for all users using a all orientation, keytype and biometric data
-for i=5:5%length(classifiers)
-    for j=1:length(cancelableFunctions)        
-        main(11,classifiers{i},'',cancelableFunctions{j},'Same_Key','Scrolling',1)
-        main(11,classifiers{i},'',cancelableFunctions{j},'Same_Key','Horizontal',1)
-        
-        main(11,classifiers{i},'',cancelableFunctions{j},'Different_Key','Scrolling',1)
-        main(11,classifiers{i},'',cancelableFunctions{j},'Different_Key','Horizontal',1)
-    end
-end
-
+% for i=5:5%length(classifiers)
+%     for j=1:length(cancelableFunctions)        
+%         main(11,classifiers{i},'',cancelableFunctions{j},'Same_Key','Scrolling',1)
+%         main(11,classifiers{i},'',cancelableFunctions{j},'Same_Key','Horizontal',1)
+%         
+%         main(11,classifiers{i},'',cancelableFunctions{j},'Different_Key','Scrolling',1)
+%         main(11,classifiers{i},'',cancelableFunctions{j},'Different_Key','Horizontal',1)
+%     end
+% end
+main(16,'knn','','Interpolation','','Scrolling',1)
 % for k=1:length(keySize)
 % for i=1:1
 %     for j=1:length(cancelableFunctions)        
