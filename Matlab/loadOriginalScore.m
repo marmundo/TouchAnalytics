9d8fbe54ab95fbda6abcf23f61005d68da4bcc41
Marcelo Damaceno <marcelodamasceno@gmail.com>
2015-05-20 19:11:32 +0100

    adds a function just to load the Original Scores

---
+++ Matlab/loadOriginalScore.m
@@ -0,0 +1,14 @@
+function [ allClientScore,allImpostorScore]=loadOriginalScore(keySize,classifierName,orientation)
+allClientScore=[];
+allImpostorScore=[];
+scoreMatrixPath=[pwd(),'/ScoreMatrix/',num2str(keySize),'/',classifierName,'/',orientation,'/Original/Same_Key'];
+for user=1:41
+    % Loading score matrix
+    saveFilePath=scoreMatrixPath;
+    load(strcat(saveFilePath,'/Score_User_',num2str(user),'.mat'),'clientScore','impostorScore');
+    
+    %storing the score to plot
+    allClientScore=[allClientScore;clientScore];
+    allImpostorScore=[allImpostorScore;impostorScore];
+end
+end
