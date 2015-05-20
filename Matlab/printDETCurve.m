f80f455298e7c4f725f9030964b0bf313aa5a136
Marcelo Damaceno <marcelodamasceno@gmail.com>
2015-05-20 11:46:23 +0100

    adds warning in printDET

--- Matlab/printDETCurve.m
+++ Matlab/printDETCurve.m
@@ -1,4 +1,13 @@
 function printDETCurve(wolves, sheep, quantLines)
+% wolves= impostor scores
+% sheep= client scores
+% quantLines = quantity of DET Curves. 'This input must have the same number
+% of columns of wolves and sheep
+
+if quantLines~=length(wolves(1,:)) && quantLines~=length(sheep(1,:))
+    warning('quantLines must have the same number of columns of wolves and sheep');
+    return;
+end
 addpath('lib')
 for hold=1:quantLines
     wer(wolves(:,hold),sheep(:,hold), [],2,[],hold);
