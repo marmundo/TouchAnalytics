9289573baff6a4cffcccd1ab2111e8ea82dc1437
Marcelo Damaceno <marcelodamasceno@gmail.com>
2015-05-22 01:04:46 +0100

    cleans the code

--- Matlab/bioconvolving.m
+++ Matlab/bioconvolving.m
@@ -10,23 +10,16 @@ for sample=1:length(biometric_data(:,1))
     first=0;
     c=0;
     for i=1:length(key)-1
-        %  size=key(i+1)-key(i);
-        % if size~=0
         if first==0
             size=key(i+1)-key(i);
             c=biometric_data(sample,1:(size+key(i)));
             first=1;
-            
         else
             n=key(i)+1;
             size=key(i+1)-key(i);
             a=biometric_data(sample,n:(size+key(i)));
-            %  if ~isempty(a) & ~isempty(c)
             c=conv(c,a);
-            %   end
         end
-        %   end
-        
     end
     transformed_data=[transformed_data;c];
 end
